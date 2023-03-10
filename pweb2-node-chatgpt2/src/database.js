396bb162c58ef59c2b0c649b7a40870c177fa419
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-03-10 09:02:57 -0300

    .

--- pweb2-node-chatgpt2/src/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('alunos-chatgpt', 'root', 'mysql123', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
